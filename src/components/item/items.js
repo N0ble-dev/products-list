a9e1e4fc9d0ba9d69b783d2a6299e16dee3c62d2
Nabil Alex <nabilmohaed46@gmail.com>
2024-04-25 21:47:40 +0200

    Co-authored-by: Ahmed El Sayad <HunterXNB@users.noreply.github.com>

--- src/components/item/items.js
+++ src/components/item/items.js
@@ -1,31 +1,47 @@
-import React from 'react';
+import React from "react";
 
 const Items = (props) => {
-    const {items, del} = props;
-    let length = items.length
-    const ListItem = length ? (
-        items.map(item => {
-            return(
-                <div key={item.id} className="item">
-                    <p>{item.product}</p>
-                    <p>{item.price}</p>
-                    <p className="delete" onClick={() => del(item.id)}>&times;</p>
-                </div>
-            )
-        })
-    ) : (
-        <div className="text">There are no items, Try to add some.</div>
-    )
-    return (
-        <div>
-            <div className="header item">
-                <p>Product</p>
-                <p>Price</p>
-                <p>Edit</p>
-            </div>
-            {ListItem}
+  const { items, del, increaseQuantity, decreaseQuantity } = props;
+  let length = items.length;
+  const ListItem = length ? (
+    items.map((item) => {
+      return (
+        <div key={item.id} className="item">
+          <p>{item.product}</p>
+          <p>{item.price}</p>
+          <p className="delete" onClick={() => del(item.id)}>
+            &times;
+          </p>
+          <p>{item.quantity}</p>
+          <p
+            style={{ cursor: "pointer" }}
+            onClick={() => increaseQuantity(item.id)}
+          >
+            +
+          </p>
+          <p
+            style={{ cursor: "pointer" }}
+            onClick={() => decreaseQuantity(item.id)}
+          >
+            -
+          </p>
         </div>
-    )
-}
+      );
+    })
+  ) : (
+    <div className="text">There are no items, Try to add some.</div>
+  );
+  return (
+    <div>
+      <div className="header item">
+        <p>Product</p>
+        <p>Price</p>
+        <p>Remove</p>
+        <p style={{ width: "300%" }}>quantity</p>
+      </div>
+      {ListItem}
+    </div>
+  );
+};
 
-export default Items
+export default Items;
